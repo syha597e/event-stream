828a992806b970d540ecb0f6a7ee80c981bebcd6
Mark Schoene <mark.schoene@tu-dresden.de>
2024-03-18 17:31:32 +0100

    Bugfix in CropEvents: The case where len(events) equals the max number of events was not handled correctly

--- s5/transform.py
+++ s5/transform.py
@@ -1,7 +1,4 @@
 import numpy as np
-import jax
-import jax.numpy as jnp
-from typing import Union, Tuple
 
 
 class Identity:
@@ -20,7 +17,7 @@ class CropEvents:
         self.num_events = num_events
 
     def __call__(self, events):
-        if self.num_events > len(events):
+        if self.num_events >= len(events):
             return events
         else:
             start = np.random.randint(0, len(events) - self.num_events)
